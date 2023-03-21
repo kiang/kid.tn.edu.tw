364ef522cc40be08e950b48ece7819291de939a9
kiang <kiange@gmail.com>
2023-03-21 14:20:33 +0800

    2023 data

--- 01_fetch_data.php
+++ 01_fetch_data.php
@@ -7,9 +7,12 @@ use Symfony\Component\DomCrawler\Field\InputFormField;
 
 $client = new Client();
 $crawler = $client->request('GET', 'https://kid.tn.edu.tw/KidAdm/Public/Reg1Stat.aspx');
-$form = $crawler->selectButton('查詢')->form();
-$btn = $form->get('ctl00$MainContent$btnQry');
-if ($btn->isDisabled()) {
+$form = $crawler->filter('form')->form();
+$btn = false;
+if ($crawler->filter('#ctl00\\$MainContent\\$btnQry')->count() > 0) {
+    $btn = $form->get('ctl00$MainContent$btnQry');
+}
+if (false === $btn || $btn->isDisabled()) {
     $domdocument = new \DOMDocument;
     $ff = $domdocument->createElement('input');
     $ff->setAttribute('name', 'ctl00$MainContent$btnQry');
@@ -17,31 +20,34 @@ if ($btn->isDisabled()) {
     $formfield = new InputFormField($ff);
     $form->set($formfield);
 }
-$areas = array('七股區', '下營區', '大內區', '山上區', '中西區', '仁德區', '六甲區',
+$areas = array(
+    '七股區', '下營區', '大內區', '山上區', '中西區', '仁德區', '六甲區',
     '北門區', '北區 ', '左鎮區', '永康區', '玉井區', '白河區', '安平區', '安定區', '安南區',
     '西港區', '佳里區', '官田區', '東山區', '東區 ', '南化區', '南區 ', '後壁區', '柳營區',
     '將軍區', '麻豆區', '善化區', '新化區', '新市區', '新營區', '楠西區', '學甲區', '龍崎區',
-    '歸仁區', '關廟區', '鹽水區');
+    '歸仁區', '關廟區', '鹽水區'
+);
 $fh = fopen(__DIR__ . '/data/2023/data.csv', 'w');
 fputcsv($fh, array('行政區', '類型', '幼兒園', '可招生名額', '登記名額', '錄取名額', '招生簡章網址', '-'));
-foreach ($areas AS $area) {
+foreach ($areas as $area) {
     $client->submit($form, array('ctl00$MainContent$ddlArea' => $area, 'ctl00$MainContent$rbStage' => '2'));
     $y2 = getTableRows($client->getResponse()->getContent());
     if (!empty($y2)) {
-        foreach ($y2 AS $line) {
+        foreach ($y2 as $line) {
             fputcsv($fh, array_merge(array($area, '2歲'), $line));
         }
     }
     $client->submit($form, array('ctl00$MainContent$ddlArea' => $area, 'ctl00$MainContent$rbStage' => '3'));
     $y3 = getTableRows($client->getResponse()->getContent());
     if (!empty($y3)) {
-        foreach ($y3 AS $line) {
+        foreach ($y3 as $line) {
             fputcsv($fh, array_merge(array($area, '3歲以上'), $line));
         }
     }
 }
 
-function getTableRows($c) {
+function getTableRows($c)
+{
     if (false !== strpos($c, '<font color="Red">查無資料!!')) {
         return [];
     }
@@ -52,9 +58,9 @@ function getTableRows($c) {
     $posEnd = strpos($c, '</table>', $pos);
     $lines = explode('</tr>', substr($c, $pos, $posEnd - $pos));
     $result = array();
-    foreach ($lines AS $line) {
+    foreach ($lines as $line) {
         $cols = explode('</td>', $line);
-        foreach ($cols AS $k => $v) {
+        foreach ($cols as $k => $v) {
             $cols[$k] = trim(strip_tags($v));
         }
         if (count($cols) === 6) {
--- 01_fetch_nonprofit.php
+++ 01_fetch_nonprofit.php
@@ -7,9 +7,12 @@ use Symfony\Component\DomCrawler\Field\InputFormField;
 
 $client = new Client();
 $crawler = $client->request('GET', 'https://kid.tn.edu.tw/NonProfit/Public/Reg1Stat.aspx');
-$form = $crawler->selectButton('查詢')->form();
-$btn = $form->get('ctl00$MainContent$btnQry');
-if ($btn->isDisabled()) {
+$form = $crawler->filter('form')->form();
+$btn = false;
+if ($crawler->filter('#ctl00\\$MainContent\\$btnQry')->count() > 0) {
+    $btn = $form->get('ctl00$MainContent$btnQry');
+}
+if (false === $btn || $btn->isDisabled()) {
     $domdocument = new \DOMDocument;
     $ff = $domdocument->createElement('input');
     $ff->setAttribute('name', 'ctl00$MainContent$btnQry');
@@ -17,7 +20,7 @@ if ($btn->isDisabled()) {
     $formfield = new InputFormField($ff);
     $form->set($formfield);
 }
-$areas = array('七股區', '永康區', '安平區', '安南區', '東區 ', '南區 ', '新營區', '麻豆區',);
+$areas = array('永康區', '安平區', '安南區', '東區 ', '南區 ', '新營區', '麻豆區',);
 $fh = fopen(__DIR__ . '/data/2023/nonprofit.csv', 'w');
 fputcsv($fh, array('行政區', '類型', '幼兒園', '可招生名額', '登記名額', '錄取名額', '招生簡章網址', '-'));
 foreach ($areas AS $area) {
